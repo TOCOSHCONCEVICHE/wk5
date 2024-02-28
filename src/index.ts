c781f68463864c3ad4a61abe2d9becc304f6cf20
mjsanchez@york.edu <mjsanchez@york.edu>
2024-02-27 21:11:43 -0600

    npm init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('It worked!');
+import { Rectangle } from './rectangle';
+import { Circle } from './circle';
+
+
+const rectangle = new Rectangle(5, 3);
+const circle = new Circle(4);
+
+// Use classes, variables, and functions
+console.log("Rectangle Area:", rectangle.area());
+console.log("Rectangle Perimeter:", rectangle.perimeter());
+rectangle.print();
+
+console.log("Circle Area:", circle.area());
+console.log("Circle Perimeter:", circle.perimeter());
+circle.print();
